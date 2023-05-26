e958e8f607ab3da9eb4f7a6d9d2f22e96813a33e
Jacek Rys <jacek@myosolutions.pl>
2023-05-26 16:12:54 +0200

    Add initial implementation of grayscale image processing Remove redundant dependencies

--- rust/image_processing/src/lib.rs
+++ rust/image_processing/src/lib.rs
@@ -1,4 +1,33 @@
+extern crate image;
+extern crate jni;
+
+use jni::JNIEnv;
+use jni::objects::{JClass, JByteBuffer};
+use jni::sys::{jbyteArray, jint};
+use image::{GenericImageView, GenericImage, FilterType, DynamicImage};
+use image::imageops::colorops::grayscale;
+
+use std::os::raw::c_void;
+use std::slice;
+
 #[no_mangle]
-pub extern "C" fn add_two_ints(a: i32, b: i32) -> i32 {
-    a + b
+pub extern "system" fn Java_com_example_MyActivity_grayscaleFilter(
+    env: JNIEnv,
+    _class: JClass,
+    image_data: JByteBuffer,
+    width: jint,
+    height: jint
+) -> jbyteArray {
+    let image_data_ptr = env.get_direct_buffer_address(image_data).unwrap();
+    let image_slice = unsafe { slice::from_raw_parts_mut(image_data_ptr as *mut u8, (width * height * 4) as usize) };
+
+    let img = image::ImageBuffer::from_raw(width as u32, height as u32, image_slice.to_vec()).unwrap();
+    let gray_img = grayscale(&img);
+
+    let gray_image_data = gray_img.into_raw();
+
+    // Convert gray_image_data back to jbyteArray to return to Java
+    let output_image = env.byte_array_from_slice(&gray_image_data).unwrap();
+
+    output_image
 }
